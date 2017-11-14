f0623aa7574ab95111d78146e688bc83040dc1f2
Omar Dhorajiwala <omardhorajiwala410@gmail.com>
2017-11-14 23:47:59 +0000

    lab 7

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -1,6 +1,6 @@
 module CurrentCart
 	
-	  private
+private
 	
 	    def set_cart 
 	      @cart = Cart.find(session[:cart_id])
@@ -8,4 +8,4 @@ module CurrentCart
 	      @cart = Cart.create
 	      session[:cart_id] = @cart.id
 	    end
-	end
+end
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -23,14 +23,12 @@ class LineItemsController < ApplicationController
   def edit
   end
 
-  # POST /line_items
-  # POST /line_items.json
-  def create
-    product = Product.find(params[:product_id])
-	@line_item = @cart.line_items.build(product: product)
-   # @line_item = LineItem.new(line_item_params)
-
-    respond_to do |format|
+ def create
+     product = Product.find(params[:product_id])
+     #@line_item = @cart.add_product(product)
+     @line_item = @cart.line_items.build(product: product)
+ 
+     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
         #format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
